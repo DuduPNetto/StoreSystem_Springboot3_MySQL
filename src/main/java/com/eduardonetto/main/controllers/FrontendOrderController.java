e289efe2ebcf2662cd1832d17e76c04540a4ff0c
Eduardo Netto <dudupnetto.en@gmail.com>
2023-07-16 09:37:50 -0300

    Showing orders

--- src/main/java/com/eduardonetto/main/controllers/FrontendOrderController.java
+++ src/main/java/com/eduardonetto/main/controllers/FrontendOrderController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.eduardonetto.main.entities.Order;
 import com.eduardonetto.main.services.OrderService;
@@ -19,9 +20,17 @@ public class FrontendOrderController {
 	private OrderService orderService;
 
 	@GetMapping
-	public String index(Model model) {
+	public String orders(Model model) {
 		List<Order> orders = orderService.findAll();
 		model.addAttribute("orders", orders);
+		return "index";
+	}
+	
+
+	@GetMapping("/find/")
+	public String findById(Model model, @RequestParam(value = "id") Long id) {
+		Order order = orderService.findById(id);
+		model.addAttribute("orders", order);
 		return "index";
 	}
 
